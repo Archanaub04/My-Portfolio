0cc35210222d5fb228e86fc52b20fbcd37f1f575
Archana U B <archanaub04@gmail.com>
2025-07-06 20:09:39 +0530

    router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,14 +10,14 @@ function App() {
   return (
     <>
       <Toaster />      
-      <BrowserRouter basename="/My-Portfolio/">
+      <HashRouter basename="/My-Portfolio/">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/projects/:slug" element={<ProjectDetail />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }

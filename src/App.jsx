2095be2874dfc76804fdfbb4f447ea78a8a5932e
Archana U B <archanaub04@gmail.com>
2025-07-06 11:54:55 +0530

    added subpathname for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/My-Portfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
